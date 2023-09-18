a7154e40a3fa91411a664a2b0742e60d5e7c244a
powerslam <timsort@kookmin.ac.kr>
2023-09-18 20:13:55 +0900

    주요 코드 라이브러리화

---
+++ VLF/VLF.cpp
@@ -0,0 +1,63 @@
+#include "Arduino.h"
+#include "Servo.h"
+#include "VLF.h"
+
+Servo SteeringServo;
+const 
+
+void vlf_init(){
+  Serial.begin(115200);
+
+  // 전면 초음파
+  pinMode(FRONT_TRIG, OUTPUT);
+  pinMode(FRONT_ECHO, INPUT);
+  
+  // 좌측 초음파
+  pinMode(LEFT_TRIG, OUTPUT);
+  pinMode(LEFT_ECHO, INPUT);
+
+  // 우측 초음파
+  pinMode(RIGHT_TRIG, OUTPUT);
+  
+  pinMode(RIGHT_ECHO, INPUT);
+
+  // 모터(DC)
+  pinMode(MOTOR_DIR, OUTPUT);
+  pinMode(MOTOR_PWM, OUTPUT);
+
+  // 모터(서보)
+  SteeringServo.attach(RC_SERVO_PIN);
+  // 서보 정중앙 설정
+  SteeringServo.write(NEURAL_ANGLE);
+}
+
+long get_ult(int trig, int echo){
+  digitalWrite(trig, LOW);
+  delayMicroseconds(2);
+
+  digitalWrite(trig, HIGH);
+  delayMicroseconds(10);
+
+  digitalWrite(trig, LOW);
+
+  return pulseIn(echo, HIGH) * 17 / 1000; 
+}
+
+void motor_control(uint8_t dir, int speed){
+  speed = max(speed, 0);
+  speed = min(speed, 255);
+
+  digitalWrite(MOTOR_DIR, dir);
+  analogWrite(MOTOR_PWM, speed);
+}
+
+void motor_stop(){
+  motor_control(FRONT_SPIN, 0);
+}
+
+void steering_control(int angle){
+  if(angle < 0) angle = max(angle, LEFT_MAX_STEER_ANGLE);
+  else if(angle > 0) angle = min(angle, RIGHT_MAX_STEER_ANGLE);
+
+  SteeringServo.write(NEURAL_ANGLE + angle);
+}
---
+++ VLF/VLF.h
@@ -0,0 +1,46 @@
+#ifndef VLF_H
+#define VLF_H
+
+// pinMode 및 변수 초기화
+void init();
+
+/// 초음파 핀 번호 ///
+#define FRONT_TRIG 9
+#define FRONT_ECHO 10
+
+#define LEFT_TRIG 48
+#define LEFT_ECHO 49
+
+#define RIGHT_TRIG 46
+#define RIGHT_ECHO 47
+/// 초음파 핀 번호(앞, 좌, 우 순) ///
+
+// 초음파 거리 재기
+// param
+// 1. trig: trig 핀 번호
+// 2. echo: echo 핀 번호
+long get_ult(int trig, int echo);
+
+/// 모터(DC) 핀 번호 ///
+#define MOTOR_DIR 4
+#define MOTOR_PWM 5
+
+#define FRONT_SPIN HIGH
+#define BACK_SPIN LOW
+/// 모터(DC) 핀 번호 ///
+
+/// 모터 제어 함수 ///
+void motor_control(uint8_t dir, int speed);
+void motor_stop();
+/// 모터 제어 함수 ///
+
+/// 서보 모터 핀 번호 및 각도 ///
+#define RC_SERVO_PIN 8
+#define NEURAL_ANGLE 90
+#define LEFT_MAX_STEER_ANGLE -70
+#define RIGHT_MAX_STEER_ANGLE 70
+/// 서보 모터 핀 번호 및 각도 ///
+
+void steering_control(int angle);
+
+#endif
