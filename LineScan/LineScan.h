2e5cc6a6acece96bb064c6077770268b5851eb56
powerslam <timsort@kookmin.ac.kr>
2023-09-26 00:40:04 +0900

    라이브러리 완성

--- LineScan/LineScan.h
+++ LineScan/LineScan.h
@@ -8,7 +8,9 @@
 //// 카메라 핀번호 ////
 
 /// 입력 데이터 수 == 1Line 이고, 128 pixel을 받아옴 ///
+#define ROI 14
 #define NPIXELS 128
+#define MID_PIXEL_NUM (NPIXELS + ROI) >> 1
 
 //// 빠른 아날로그 입력 ////
 #define FASTADC 1
@@ -19,18 +21,26 @@
 
 class LineScan {
   public:
-    int LineSensor_Data_Adaption[NPIXELS];
-
-    LineScan(int thresholdValue);
+    short midPos;
+  
+  public:
+    LineScan();
 
     void init();
-    void read_line_sensor();
-    void calcCentroid();
+    void read_cam();
+    void calc_centroid(short threshold);
+
+    short get_pixel(int i){
+      return Pixel[i];
+    }
+
+    short get_diff_pixel(int i){
+      return diffPixel[i];
+    }
 
   private:
-    byte Pixel[NPIXELS];
-    int LineSensor_Data[NPIXELS];
-    int thresholdValue, MaxPixel, MinPixel;
+    short Pixel[NPIXELS], diffPixel[NPIXELS];
+    short leftPos, rightPos;
 };
 
 #endif
