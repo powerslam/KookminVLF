b9af5aabc8604365421010fcf671d0966fbc0a87
powerslam <timsort@kookmin.ac.kr>
2023-11-14 19:05:19 +0900

    메인 코드 수정

--- total/total.ino
+++ total/total.ino
@@ -5,8 +5,16 @@
 #define MAZE_MODE 1
 #define OBSTACLE_MODE 2
 
-#define P_TERM 1.6
-#define D_TERM 0.5
+#define THRESHOLD 50
+
+// 원래는 차례대로 1.6, 0, 0.5 임
+#define P_TERM 1.5
+#define I_TERM 0
+#define D_TERM 0.6
+
+// ==> 근데 이 친구가 영점이 왼쪽으로 가 있음 ==> 어떻게 해결할까?
+// 생각에는 중심점에 대해 bias를 줘야 할 거 같은데 어렵다
+// 라인이 쓸데없는 걸 체크하는 것 같기도? ==> processing 찍어봐야 함
 
 LineScan cam;
 
@@ -31,10 +39,9 @@ void setup() {
   motor_control(FRONT_SPIN, 100);
 
   // 첫 시작은 NORMAL_MODE(라인 주행)
-  mode = OBSTACLE_MODE; // NORMAL_MODE;
+  mode = NORMAL_MODE;
   change_mode = false;
 }
-
 
 long distance = 0;
 long get_ult(int trig, int echo) {
@@ -51,67 +58,101 @@ long get_ult(int trig, int echo) {
   return distance;
 }
 
+void blink(int dt){
+  digitalWrite(13, HIGH);
+  delay(dt);
+  digitalWrite(13, LOW);
+  delay(dt);
+}
+
 // 각도 구하기
 int get_angle(){
   cam.read_cam();
-  cam.calc_centroid(50);
+  cam.calc_centroid(THRESHOLD);
 
   angle_error_prev = angle_error;
   angle_error = cam.midPos - MID_PIXEL_NUM;
 
-  long angle = P_TERM * angle_error + D_TERM * (angle_error - angle_error_prev);
-  angle = constrain(angle, -82, 92);
+  long p_value = P_TERM * angle_error;
+  long i_value = I_TERM * angle_error;
+  long d_value = D_TERM * (angle_error - angle_error_prev);
+
+  long angle = p_value + i_value + d_value;
+  angle = constrain(angle, -82, 98); // 중앙 각도가 82 임
   return angle;
 }
 
 void loop(){switch(mode){
 case NORMAL_MODE: // 일반 주행 모드
+  // 만약에 오른쪽 센서에 40cm 미만이 체크되면
+  if(get_ult(RIGHT_TRIG, RIGHT_ECHO) < 40){
+    mode = MAZE_MODE;
+    steering_control(0);
+    break;
+  } // 미로 진입
+
   steering_control(get_angle());
-  digitalWrite(13, HIGH);
-  // 한 줄이 전부 흰색이면
-  // while(cam.chk_line()) change_mode = true;
-  // if(change_mode){
-  //   change_mode = false;
-  //   robot_mode = MAZE_MODE;
-  //   // motor_stop(0); // @have to test
-  //   break;
-  // } // 미로 진입 시
-  
-  delay(50);
+  delay(10);
   break;
 
 case MAZE_MODE:
-  // 미로의 경우 항상 흰색 라인에 간다는 보장이 없음...
-  // while(cam.chk_line()) change_mode = true;
-  // if(change_mode){
-  //   steering_control(0); // 중심 맞추고 전진해야 함
-  //   change_mode = false;
-  //   robot_mode = OBSTACLE_MODE;
-  //   break;
-  // } // 탈출 시
-  // // 그 pwa 적용하기(라이브러리)
+  // //////////////// 미로 주행하기 ////////////////////
+  digitalWrite(13, HIGH); // 미로 들어왔다는 신호
+  motor_control(HIGH, 180);
 
-  right_obstacle = get_ult(RIGHT_TRIG, RIGHT_ECHO);
+  int mz_cnt = 0;
+
+  bool mz_flag = false;
+  bool mz_flag_prev = false;
+
+  while(mz_cnt < 2){
+    long right = get_ult(RIGHT_TRIG, RIGHT_ECHO);
+    
+    // mz_flag: 현재 오른쪽이 90 이상인 경우 ==> 코너에 도달한 경우
+    mz_flag_prev = mz_flag;
+    mz_flag = right >= 90;
+
+    // 생각해보니까 !mz_flag_prev && mz_flag 가 맞는 조건인 거 같음
+    // 이제 막 코너에 도달한 경우
+    if(mz_flag_prev != mz_flag) {      
+      motor_control(HIGH, 130);
+
+      // 여기는 고정 각도로 꺾음 ==> 라인 진입을 수월하게 하기 위해서
+      while(get_ult(RIGHT_TRIG, RIGHT_ECHO) > 50) 
+        steering_control(RIGHT_STEER_ANGLE);
+      
+      mz_cnt++; // 코너 카운트 1 증가
+      mz_flag_prev = mz_flag = false; // 위에 조건이 채택되면 이 친구는 없어도 됨
+    }
+
+    // 일반적인 상황에서는 미로 벽과 20cm 간격을 두고 주행
+    else {
+      long error = get_ult(RIGHT_TRIG, RIGHT_ECHO) - 20; // 0 ~ 10
+      steering_control(constrain(error * 6, -82, 98));
+      delay(50);
+    }
+  }
   
-  //////////////// 미로 주행하기 ////////////////////
-  if (right_obstacle > 15) {
-    // Serial.println("Case 1");
-    steering_control(RIGHT_STEER_ANGLE - 10);
-    delay(200);
+  // 코너를 2번 주행하면 LED를 끔
+  digitalWrite(13, LOW);
+
+  // LED 깜빡이면서 무한대기
+  while(1){
+    motor_stop();
+    steering_control(0);
+
+    digitalWrite(13, HIGH);
+    delay(500);
+
+    digitalWrite(13, LOW);
+    delay(500);
   }
 
-  else if (right_obstacle < 10) {
-    // Serial.println("Case 2");
-    steering_control(LEFT_STEER_ANGLE + 40);
-    delay(200);
-  }
+  // 만약에 된다면 //
+  motor_control(HIGH, 100);
+  delay(500);
 
-  // ==> 해보고 되면 테스트 해보기
-  /*
-  long error = right_obstacle - 10; // 0 ~ 10
-  steering_control(error * 6);
-  delay(100);
-  */
+  mode = OBSTACLE_MODE;
   //////////////// 미로 주행하기 ////////////////////
   break;
 
